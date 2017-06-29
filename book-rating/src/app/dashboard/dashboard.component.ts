0179ffe0f544bb7e7e2700885aa767030934612b
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-06-29 15:12:53 +0200

    unsubscribe, shows hearts for rating

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { BookComponent } from './../book/book.component';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Book } from './../shared/book';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
 
   books: Book[]; // vorher: string[]
+
+  @ViewChild(BookComponent)
+  firstBookComponent: BookComponent;
+
+  subscribtion: Subscription;
 
   constructor() { }
 
@@ -20,8 +29,21 @@ export class DashboardComponent implements OnInit {
     this.reorderBooks(null);
   }
 
+  ngAfterViewInit() {
+    this.subscribtion = this.firstBookComponent.rated
+      .filter(b => b.title === 'Angular')
+      .map(b => b.title)
+      .subscribe((title) => {
+        console.log(title)
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscribtion.unsubscribe();
+  }
+
   reorderBooks(book: Book) {
-    console.log('Hallo Buch:', book);
+    // console.log('Hallo Buch:', book);
     this.books.sort((a, b) => b.rating - a.rating);
   }
 }
